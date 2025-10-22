20cdd0316418e854a1aa13e7c64d4b299f50b4cc
Ditt namn <din.email@example.com>
2025-10-22 11:17:11 +0200

    lagt till global using

---
+++ AlbertBankApp/GlobalUsing.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.ComponentModel.DataAnnotations;
+global using AlbertBankApp.Domain;
+global using AlbertBankApp.Interfaces;
+global using NavigationManager = Microsoft.AspNetCore.Components.NavigationManager;
